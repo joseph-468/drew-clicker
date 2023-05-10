b66e27ff16298205533beed2009388efaebf2d42
Joseph <josephalba2006@outlook.com>
2023-05-10 14:15:03 +0100

    Spawned Drew

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use bevy::window::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).add_startup_system(setup).run()
+    App::new().add_plugins(DefaultPlugins).add_startup_system(setup).add_startup_system(spawn_camera).run()
 }
 
 fn setup(mut commands: Commands, 
@@ -13,11 +13,23 @@ fn setup(mut commands: Commands,
     commands.spawn((
         SpriteBundle {
             transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-            texture: asset_server.load("sprites/drew-dribble.png"),
+            texture: asset_server.load("drew-dribble.png"),
             ..default() 
         },
         Drew {},
     ));
+}
+
+fn spawn_camera(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>
+    ) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
 
 #[derive(Component)]
