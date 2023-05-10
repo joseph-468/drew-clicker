37ec0d2d66f35c0280f1928950d6b7e69d301581
Joseph <josephalba2006@outlook.com>
2023-05-10 20:16:05 +0100

    Added dps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy::window::*;
+use std::time::Duration;
 
 const WINDOW_TITLE: &str = "Drew Clicker";
 const RESOLUTION_X: f32 = 1200.0;
@@ -15,9 +16,11 @@ fn main() {
         ..default()
     }))
         .add_startup_system(setup)
+        .add_startup_system(setup_dps)
         .add_startup_system(spawn_camera)
         .add_system(drew_click)
         .add_system(update_text)
+        .add_system(calculate_dps)
         .run()
 }
 
@@ -26,6 +29,8 @@ fn setup(mut commands: Commands,
          asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
+
+    // Spawn Drew & Player
     commands.spawn((
         SpriteBundle {
             transform: Transform::from_xyz(300.0, window.height() / 2.0, 0.0),
@@ -34,20 +39,19 @@ fn setup(mut commands: Commands,
         },
         Drew {},
     ));
-    commands.spawn(Player {money: 0});
+    commands.spawn(Player {droodles: 10, dps: 1});
+
+    // Spawn text
     commands.spawn((
-        // Create a TextBundle that has a Text with a single section.
         TextBundle::from_section(
-            // Accepts a `String` or any type that converts into a `String`, such as `&str`
             "Droodles: 0",
             TextStyle {
                 font: asset_server.load("fonts/font.ttf"),
                 font_size: 100.0,
                 color: Color::WHITE,
             },
-        ) // Set the alignment of the Text
-        .with_text_alignment(TextAlignment::Center)
-        // Set the style of the TextBundle itself.
+       )
+        .with_text_alignment(TextAlignment::Left)
         .with_style(Style {
             position_type: PositionType::Absolute,
             position: UiRect {
@@ -58,6 +62,28 @@ fn setup(mut commands: Commands,
             ..default()
         }),
         MoneyText {}
+    ));
+
+    commands.spawn((
+        TextBundle::from_section(
+            "DPS: 0",
+            TextStyle {
+                font: asset_server.load("fonts/font.ttf"),
+                font_size: 100.0,
+                color: Color::WHITE,
+            },
+       )
+        .with_text_alignment(TextAlignment::Left)
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                top: Val::Px(5.0),
+                left: Val::Px(15.0),
+                ..default()
+            },
+            ..default()
+        }),
+        DPSText {}
     ));
 
 }
@@ -74,10 +100,11 @@ fn spawn_camera(
     });
 }
 
-fn drew_click(buttons: Res<Input<MouseButton>>,
-              window_query: Query<&Window, With<PrimaryWindow>>,
-              mut player_query: Query<&mut Player>,) {
-
+fn drew_click(
+    buttons: Res<Input<MouseButton>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut player_query: Query<&mut Player>
+    ) {
     let window = window_query.get_single().unwrap();
     let mut player = player_query.get_single_mut().unwrap();
 
@@ -85,16 +112,45 @@ fn drew_click(buttons: Res<Input<MouseButton>>,
         if let Some(_position) = window.cursor_position() {
             let pos = window.cursor_position().unwrap();
             if pos.x >= 100.0 && pos.x <= 500.0 && pos.y >= 150.0 && pos.y <= 550.0 {
-                player.money += 1;
+                player.droodles += 1;
             }
         }
     }
 } 
 
-fn update_text(mut query: Query<&mut Text, With<MoneyText>>, player_query: Query<&Player>) {
-    let money = player_query.get_single().unwrap().money;
-    for mut text in &mut query {
-        text.sections[0].value = format!("Droodles: {money}");
+fn update_text(
+        mut droodle_query: Query<&mut Text, (With<MoneyText>, Without<DPSText>)>,
+        mut dps_query: Query<&mut Text, (With<DPSText>, Without<MoneyText>)>,
+        player_query: Query<&Player>
+        ) {
+    let droodles = player_query.get_single().unwrap().droodles as f64 /  10.0;
+    let dps = player_query.get_single().unwrap().dps as f64 / 10.0;
+    for mut text in &mut droodle_query {
+        text.sections[0].value = format!("Droodles: {:?}", droodles);
+    }
+    for mut text in &mut dps_query {
+        text.sections[0].value = format!("DPS: {:?}", dps);
+    }
+
+}
+
+fn setup_dps(
+    mut commands: Commands,
+    ) {
+    commands.insert_resource(DPSTime {
+        timer: Timer::new(Duration::from_millis(100), TimerMode::Repeating),
+    })
+}
+
+fn calculate_dps(
+    mut player_query: Query<&mut Player>,
+    mut dps_timer: ResMut<DPSTime>,
+    time: Res<Time>,
+    ) {
+    let mut player = player_query.get_single_mut().unwrap();
+    dps_timer.timer.tick(time.delta());
+    if dps_timer.timer.finished() {
+        player.droodles += player.dps;
     }
 }
 
@@ -103,8 +159,15 @@ struct Drew {}
 
 #[derive(Component)]
 struct Player {
-    money: u128
+    droodles: u128,
+    dps: u128, 
 }
 
 #[derive(Component)]
 struct MoneyText {}
+
+#[derive(Component)]
+struct DPSText {}
+
+#[derive(Resource)]
+struct DPSTime {timer: Timer}
