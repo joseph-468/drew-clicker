47324a688ffa444ca20816c247082473d56db5f4
Joseph <josephalba2006@outlook.com>
2023-05-10 14:05:00 +0100

    Added setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use bevy::prelude::*;
+use bevy::window::*;
 
 fn main() {
-    App::new().run()
+    App::new().add_plugins(DefaultPlugins).add_startup_system(setup).run()
 }
+
+fn setup(mut commands: Commands, 
+         window_query: Query<&Window, With<PrimaryWindow>>,
+         asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/drew-dribble.png"),
+            ..default() 
+        },
+        Drew {},
+    ));
+}
+
+#[derive(Component)]
+struct Drew {}
 
